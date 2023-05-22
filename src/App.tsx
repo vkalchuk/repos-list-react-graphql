fcb65afeb2c41a38421b3ef3dbd54538e8170d0e
Vitalii Kalchuk <v.sweak@gmail.com>
2023-05-22 18:18:18 +0200

    run linter and tests for precommit hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { NetworkStatus, useLazyQuery } from '@apollo/client'
 import { GET_REPOS } from './graphql/queries'
 import RepositoryList from './components/RepositoryList/RepositoryList'
